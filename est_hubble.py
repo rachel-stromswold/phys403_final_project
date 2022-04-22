dea4e90295d9128af8108d6ce5248f596bfc0323
rachel-stromswold <rachel.stromswold@gmail.com>
2022-04-22 10:22:39 -0400

    switched to hdf5 format

--- est_hubble.py
+++ est_hubble.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from scipy import integrate
+import h5py
 
 #np.seterr(all='raise')
 
@@ -20,45 +21,58 @@ DIST_GW_VR = 150        # Mpc (variance of the GW observation
 #we need to integrate over potential redshifts for each galaxy. To make this feasible we'll limit the range to some multiple of the standard deviation
 N_REDSHIFT_SIGS = 2
 
-#load the list of potential galaxies
-gal_list_fname = 'GW_Events/GW200202_gals.txt'
-galaxies = np.loadtxt(gal_list_fname, delimiter=' ')
+#EVENT_LIST = ['GW200202_154313-v1', 'GW200115_042309-v2', 'GW200208_222617-v1', 'GW190814_211039-v3']
+EVENT_LIST = ['GW200202_154313-v1']
 
 #prior range on the hubble constant
 h_vals = np.linspace(PRIOR_RANGE[0], PRIOR_RANGE[1])
 pdf = np.zeros(len(h_vals))
 
-def integrand(z_i, z_mu, z_err_sq, h_0):
+def integrand(z_i, z_mu, z_err_sq, h_0, gw_dist_mu, gw_dist_var):
     '''integrand for Bayes' theorem
     z_i: the term being integrated over, the redshift of the ith galaxy
     z_mu: expected value of z based on EM observation
     z_err_sq: variance on z (taken to be Gaussian)
     sig_cz_sq: the square error of c*z_i
+    gw_dist_mu: expected value of GW distance
+    gw_dist_var: variance of GW distance
     h_0: Hubble's constant
     '''
     #we need to marginalize over dzi and solid angles (see Soares-Santos et al)
     c_z = C_L*z_i
-    try:
+    '''try:
         dV = c_z*c_z / (h_0*h_0*h_0*math.sqrt(OMEGA_M*(1+z_i)**3 + OMEGA_A))
     except ValueError:
-        dV = 1
+        dV = 1'''
+    dV = 1
     #compute marginal posteriors on the GW data and EM data
-    marg_gw = math.exp( (h_0*DIST_GW_MU - c_z)*(c_z - h_0*DIST_GW_MU) / (2*DIST_GW_VR*h_0*h_0) )
+    marg_gw = math.exp( (h_0*gw_dist_mu - c_z)*(c_z - h_0*gw_dist_mu) / (2*gw_dist_var*h_0*h_0) )
     marg_em = math.exp( (z_i - z_mu)*(z_mu - z_i) / (2*z_err_sq) )
     return marg_gw*marg_em*dV
 
-#posterier from Nair et al
-for gal in galaxies:
-    mu_z = gal[2]
-    sig_z = gal[3] #error on the redshift of galaxy i
-    var_z = gal[3]*gal[3] #error on the redshift of galaxy i
+for ev in EVENT_LIST:
+    #load the list of potential galaxies
+    rshift_fname = 'GW_Events/' + ev + '_rshifts.h5'
+    galaxies = h5py.File(rshift_fname, 'r')
 
-    #some galaxies have zero catalogued redshift and error, we ignore these to avoid divisions by zero
-    if var_z > 0:
-        #iterate over the hubble constant prior
-        for i, h in enumerate(h_vals):
-            #integrate over possible redshifts
-            pdf[i] += integrate.quad(integrand, gal[2]-N_REDSHIFT_SIGS*gal[3], gal[2]+N_REDSHIFT_SIGS*gal[3], args=(mu_z, var_z, h,))[0]/sig_z
+    #figure out the range for our z-cut
+    ev_d_range = galaxies['distance_posterior']['GW_distance']['conf_interval']
+    z_min = ev_d_range[0]*PRIOR_RANGE[0] / C_L
+    z_max = ev_d_range[1]*PRIOR_RANGE[1] / C_L
+
+    gw_dist_mu = galaxies['distance_posterior']['GW_distance']['expectation'][0]
+    gw_dist_var = (galaxies['distance_posterior']['GW_distance']['std_error'][0])**2
+    #posterier from Nair et al
+    rshifts = galaxies['redshifts']
+    for mu_z, sig_z in zip(rshifts['z'], rshifts['z_err']):
+        var_z = sig_z*sig_z #error on the redshift of galaxy i
+
+        #some galaxies have zero catalogued redshift and error, we ignore these to avoid divisions by zero
+        if var_z > 0:
+            #iterate over the hubble constant prior
+            for i, h in enumerate(h_vals):
+                #integrate over possible redshifts
+                pdf[i] += integrate.quad(integrand, z_min, z_max, args=(mu_z, var_z, h, gw_dist_mu, gw_dist_var))[0]/sig_z
 
 plt.plot(h_vals, pdf)
 plt.xlabel(r'$H_0$ km s$^{-1}$ Mpc$^{-1}$')
--- vizier.py
+++ vizier.py
@@ -10,10 +10,13 @@ from mocpy import MOC
 from astropy import units as u
 from astroquery.xmatch import XMatch
 
+import h5py
+
 import os
 import numpy as np
 from datetime import datetime
 import random
+import math
 
 #constants that we'll need
 C_L = 2.99792458e5      # km / s (speed of light in vacuum)
@@ -29,7 +32,7 @@ EVENT_LIST = ['GW200202_154313-v1', 'GW200115_042309-v2', 'GW200208_222617-v1', 
 WVFRM_LIST = ['C01:IMRPhenomXPHM', 'C01:IMRPhenomNSBH:HighSpin', 'C01:IMRPhenomXPHM', 'C01:SEOBNRv4PHM']
 
 #we apply a random shift factor to the redshifts to blind our analysis
-blind_fact = random.uniform(1.0, 5.0)
+blind_fact = random.uniform(0.8, 1.2)
 #we need to be able to unshift the data after we're done and repeat our analysis. No peeking!
 with open("secret.txt", 'a') as file:
     file.write("Scaling factor for events {} (Generated at {}): {}".format(EVENT_LIST, datetime.now(), blind_fact))
@@ -71,39 +74,53 @@ def get_conf_interval(samples, conf_level=0.9):
     return ex, var, arr[low_i], arr[up_i]
 
 for ev, wf in zip(EVENT_LIST, WVFRM_LIST):
-    #check if we have the fits file for the event, create it if we don't
-    ev_fname = search_local(ev)
-    fits_fname = 'GW_Events/' + ev + '.fits'
-    #if not os.path.exists(fits_fname):
-    if ev_fname == '':
-        #download the GW data, this is in an unhelpful format so we save it to a temporary directory and then convert it
-        data = fetch_open_samples(ev, read_file=True, outdir='/tmp')
-    else:
-        data = pesummary.io.read('GW_Events/' + ev_fname)
-    
-    #find the skymap from the GW data
-    skymap = data.skymap[wf]
-    uniq = np.array([i for i in range(len(skymap))])
-    ligo_sky = MOC.from_valued_healpix_cells(uniq, skymap, cumul_to=SKYLOC_CONF_LEVEL)
+    rshift_fname = 'GW_Events/' + ev + '_rshifts.h5'
+    if not os.path.exists(rshift_fname):
+        #check if we have the fits file for the event, create it if we don't
+        ev_fname = search_local(ev)
 
-    #read from the vizier SDSS galaxy catalog
-    print("Now finding galaxies in confidence interval")
-    sdss_match = ligo_sky.query_vizier_table('V/147/sdss12', max_rows=N_SURVEY_ROWS)
+        if ev_fname == '':
+            #download the GW data, this is in an unhelpful format so we save it to a temporary directory and then convert it
+            data = fetch_open_samples(ev, read_file=True, outdir='/tmp')
+        else:
+            data = pesummary.io.read('GW_Events/' + ev_fname)
+        
+        #find the skymap from the GW data
+        skymap = data.skymap[wf]
+        uniq = np.array([i for i in range(len(skymap))])
+        ligo_sky = MOC.from_valued_healpix_cells(uniq, skymap, cumul_to=SKYLOC_CONF_LEVEL)
 
-    #now we need to convert our distance cut for the event into a redshift cut using extreme values (z= d_L*H_0/c e.g. the max value z may take is d_L,max*H_0,max/c)
-    dist_ex, dist_var, dist_lo, dist_hi = get_conf_interval(data.samples_dict[wf]["luminosity_distance"])
-    z_lo = dist_lo*PRIOR_RANGE[0] / C_L
-    z_hi = dist_hi*PRIOR_RANGE[1] / C_L
+        #read from the vizier SDSS galaxy catalog
+        print("Now finding galaxies in confidence interval for " + ev + ". This could take a while.")
+        sdss_match = ligo_sky.query_vizier_table('V/147/sdss12', max_rows=N_SURVEY_ROWS)
 
-    #write the list of potential galaxies and most importantly their redshifts (with random blinding factor) to a file
-    with open('GW_Events/' + ev + '.txt', 'w') as file:
-        #write the distances
-        file.write("#GW_dist, GW_dist_var, lo_bound, up_bound")
-        file.write("{} {} {} {}".format(dist_ex*blind_fact, dist_var*blind_fact, dist_lo*blind_fact, dist_hi*blind_fact))
-        file.write("#ra dec z zErr\n")
-        for row in sdss_match:
-            #check that the object is actually a galaxy (type 3) and that it is in the 90% distance range
-            if row['class'] == 3 and row['z'] > z_lo and row['z'] < z_hi:
-                file.write( "{} {} {} {}\n".format(row['ra'], row['dec'], row['z'], row['zErr']) )
+        #now we need to convert our distance cut for the event into a redshift cut using extreme values (z= d_L*H_0/c e.g. the max value z may take is d_L,max*H_0,max/c)
+        dist_ex, dist_var, dist_lo, dist_hi = get_conf_interval(data.samples_dict[wf]["luminosity_distance"])
+        z_lo = dist_lo*PRIOR_RANGE[0] / C_L
+        z_hi = dist_hi*PRIOR_RANGE[1] / C_L
 
-print("finished saving to file")
+        #write the list of potential galaxies and most importantly their redshifts (with random blinding factor) to a file
+        print("saving to file")
+        with h5py.File(rshift_fname, 'w') as f:
+            #write the distance information
+            dist_post = f.create_group("distance_posterior")
+            dist_dset = dist_post.create_group("GW_distance")
+            dset1 = dist_dset.create_dataset("expectation", (1,), dtype='f')
+            dset1[0] = dist_ex*blind_fact
+            dset2 = dist_dset.create_dataset("std_error", (1,), dtype='f')
+            dset2[0] = math.sqrt(dist_var)*blind_fact
+            dset3 = dist_dset.create_dataset("conf_interval", (2,), dtype='f')
+            dset3[0] = dist_lo*blind_fact
+            dset3[1] = dist_hi*blind_fact
+            #write the redshifts for matching galaxies
+            match_gals = [[], []]
+            for row in sdss_match:
+                #check that the object is actually a galaxy (type 3) and that it is in the 90% distance range
+                if row['class'] == 3 and row['zsp'] > z_lo and row['zsp'] < z_hi:
+                    match_gals[0].append(row['zsp'])
+                    match_gals[1].append(row['e_zsp'])
+            rshift_grp = f.create_group('redshifts')
+            rshift_grp['z'] = np.array(match_gals[0])
+            rshift_grp['z_err'] = np.array(match_gals[0])
+
+        print("finished saving" + rshift_fname)
